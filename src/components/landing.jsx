995d56ea19a194fcf4de3812e862aaba91b7ffdc
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-06-20 22:06:40 +0530

    hmh

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -5,31 +5,41 @@ import Page from "./page2";
 import "./land.scss";
 
 export default function Landing() {
+  const li = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const li2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [transition, setTransition] = useState(false);
   const [color, setColor] = useState("black");
   const [subcolor, setSubcolor] = useState("white");
+  const [bar, setBar] = useState("#1C2321");
+  const [bg, setBg] = useState("#EEF1EF");
   const [opacity, setOpacity] = useState(0);
   const [border, setBorder] = useState("0px 0px 0px white");
   const [subrect, setSubrect] = useState("#5E6572");
   const [scale, setScale] = useState(1);
-  const [picscale, setPicscale] = useState(0);
-  const [picrotat, setPicrot] = useState(1);
   const [pageTran, setPagetran] = useState(false);
+  const [picy, setPicy] = useState(0);
+  const [picscale, setPicscale] = useState(1);
   const handlemouseenter = () => {
     setColor("white");
-    setPicrot(1.3);
-    setPicscale(-16);
     setTransition(true);
     setScale(150);
     setSubcolor("white");
     setSubrect("#7D98A1");
     setOpacity(1);
     setBorder("0px 0px 10px #7D98A1");
+    setTimeout(() => {
+      setBg("#1C2321");
+      setBar("white");
+    }, 420);
+    setPicscale(1.5);
+    setPicy(200);
   };
   const handlemouseleave = () => {
+    setPicscale(1);
+    setPicy(0);
+    setBg("#EEF1EF");
+    setBar("#1C2321");
     setColor("black");
-    setPicrot(1);
-    setPicscale(0);
     setBorder("0px 0px 0px white");
     setSubrect("#5E6572");
     setTransition(false);
@@ -106,14 +116,14 @@ export default function Landing() {
             id="more"
             onMouseEnter={handlemouseenter}
             onMouseLeave={handlemouseleave}
-            initial={{ scale:0.1, opacity: 0 }}
-              transition={{
-                type: "tween",
-                ease: "anticipate",
-                duration: 1,
-                delay: 1,
-              }}
-              animate={{ scale: 1, opacity: 1 }}
+            initial={{ scale: 0.1, opacity: 0 }}
+            transition={{
+              type: "tween",
+              ease: "anticipate",
+              duration: 1,
+              delay: 1,
+            }}
+            animate={{ scale: 1, opacity: 1 }}
           >
             <div id="nuh" style={{ color: subcolor }}>
               {" "}
@@ -130,15 +140,55 @@ export default function Landing() {
             Click!
           </div>
         )}
-        <div id="pic-container">
+        <div id="pic-container" style={{ backgroundColor: bg }}>
+          <div id="grid">
+            <div id="rows">
+              {li.map((lines, index) => {
+                return (
+                  <motion.div
+                    id="lines"
+                    style={{ backgroundColor: bar }}
+                    initial={{ width: "0px" }}
+                    transition={{
+                      type: "tween",
+                      ease: "easeInOut",
+                      delay: 1.4 + index * 0.1,
+                      duration: 0.38,
+                    }}
+                    animate={{ width: "100vw" }}
+                  ></motion.div>
+                );
+              })}
+            </div>
+            <div id="col">
+              {li2.map((lines, index) => {
+                return (
+                  <motion.div
+                    id="lines"
+                    style={{ backgroundColor: bar }}
+                    intial={{ height: "0px" }}
+                    transition={{
+                      type: "tween",
+                      ease: "easeInOut",
+                      delay: 1.4 + index * 0.1,
+                      duration: 0.38,
+                    }}
+                    animate={{ height: "100vh" }}
+                  ></motion.div>
+                );
+              })}
+            </div>
+          </div>
           <motion.img
-            // style={{ transform: `rotate(${picscale}deg) scale(${picrotat})` }}
             src="/nihal.png"
             alt="me"
             initial={{ x: -500 }}
-            transition={{type:"tween",ease:"easeInOut",duration:0.7}}
-            // animate={{ x: 0,rotate: picscale, scale: picrotat }}
-            animate={{ x: 0, }}
+            transition={{
+              x: { type: "tween", ease: "easeInOut", duration: 0.7 },
+              y: {  type:"tween",duration: 0.1 },
+              scale: { type:"tween", duration: 0.1 },
+            }}
+            animate={{ x: 0, y: picy, scale: picscale }}
           />
         </div>
       </div>
