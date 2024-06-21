8bf9f92817b994a074f244d7a81947fb35c6e9cc
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-06-21 15:30:28 +0530

    transition-done

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+export default function Page({ trans }) {
+  const [curr, setCurr] = useState("About Me");
+  const ind = ["Content", "About Me", "Tech Stack", "Projects", "Contact"];
 
-export default function Page({trans}){
-    return(
-        <div id="page-container" style={{transform:`translateY(${trans})`}}>nuh uh</div>
-    )
+  return (
+    <div id="page-container" style={{ transform: `translateY(${trans})` }}>
+      <div id="bars-container">
+        <div id="index">
+          {ind.map((content, index) => {
+            return (
+              <motion.div
+                id="each-content"
+                style={{
+                  color:
+                    content == curr || content == "Content" ? "white" : "grey",
+                  cursor: content == "Content" ? "default" : "pointer",
+                }}
+                initial={{ opacity: 0, x: 0 }}
+                transition={{ type: "tween", delay: 3, duration: 0.38 }}
+                animate={{ opacity: 1, x: index % 2 == 0 ? -100 : 100 }}
+                onClick={() => setCurr(content)}
+              >
+                {content}
+                {curr == content && curr!="Content" && (
+                  <motion.div
+                    id="underline"
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                  ></motion.div>
+                )}
+              </motion.div>
+            );
+          })}
+        </div>
+        <div id="bars">
+          <motion.div
+            id="bar"
+            animate={{ rotate: "45deg", y: -90, x: -20 }}
+            transition={{ type: "tween", delay: 2.1, duration: 0.38 }}
+          ></motion.div>
+          <motion.div
+            id="bar"
+            animate={{ rotate: "45deg", y: 90, x: 20 }}
+            transition={{ type: "tween", delay: 2.1, duration: 0.38 }}
+          ></motion.div>
+        </div>
+      </div>
+      <motion.div
+        id="content-container"
+        initial={{ opacity: 0, y: 56 }}
+        transition={{ type: "tween", delay: 3.5, duration: 0.29 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Content soon
+      </motion.div>
+    </div>
+  );
 }
